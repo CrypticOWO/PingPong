0f810682ec43ed3f55478bba373be40f68b14051
CrypticOWO <zhao.ryan.77@gmail.com>
2023-09-14 12:09:47 -0400

    Player 2 and stuff :>

--- My project/Assets/Ball movement/Ballmovement.cs
+++ My project/Assets/Ball movement/Ballmovement.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ballmovement : MonoBehaviour
 {
     public float xSpeed = 0; //variable for x movement
     public float ySpeed = 0; //variable for y movement
 
-    private float xRightBorder = 9.5f; //varible for x dimension border of the game
-    private float yUpBorder = 7.5f; //varible for y dimension border of the game
-    private float xLeftBorder = -9.5f; //varible for x dimension border of the game
-    private float yDownBorder = -7.5f; //varible for y dimension border of the game
+    private float xBorder = 8f; //varible for x dimension border of the game
+    private float yBorder = 4.5f; //varible for y dimension border of the game
 
     //variable for move state
     public bool xMove = true;
     public bool yMove = true;
 
+    //variables for score
+    int playerOneScore;
+    public Text scoreTextP1;
+
+    int playerTwoScore;
+    public Text scoreTextP2;
+
     // Start is called before the first frame update
     void Start()
     {
-        xSpeed = 0.01f;
+        xSpeed = 0.02f;
         ySpeed = 0.01f;
     }
 
@@ -28,29 +34,45 @@ public class Ballmovement : MonoBehaviour
     {
         if (xMove == true)
         {
-            transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y + ySpeed); //move right
+            transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y); //move right
         }
         else
         {
-            transform.position = new Vector2(transform.position.x - xSpeed, transform.position.y - ySpeed); //move left
+            transform.position = new Vector2(transform.position.x - xSpeed, transform.position.y); //move left
         }
-        
-        if (transform.position.x >= xRightBorder)
+
+        if (yMove == true)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + ySpeed); //move right
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - ySpeed); //move left
+        }
+
+        if (transform.position.x >= xBorder)
         {
             xMove = false;
+            playerOneScore += 1;
+            
         }
-        if (transform.position.y >= yUpBorder)
+        if (transform.position.y >= yBorder)
         {
             yMove = false;
         }
-        if (transform.position.x <= xLeftBorder)
+        if (transform.position.x <= -xBorder)
         {
             xMove = true;
+            playerTwoScore += 1;
         }
-        if (transform.position.y <= yDownBorder)
+        if (transform.position.y <= -yBorder)
         {
             yMove = true;
         }
+
+        scoreTextP1.text = playerOneScore.ToString();
+
+        scoreTextP2.text = playerTwoScore.ToString();
     }
     void OnCollisionEnter2D(Collision2D collision) //when a collision happens
     {
--- My project/Assets/Paddle Movement/paddlemovement2.cs
+++ My project/Assets/Paddle Movement/paddlemovement2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class paddlemovement : MonoBehaviour
+public class paddlemovement2: MonoBehaviour
 {
     public float ySpeed = 0.1f;
     public float yBorder = 3.5f;
@@ -10,17 +10,17 @@ public class paddlemovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < yBorder)
+        if (Input.GetKey(KeyCode.W) && transform.position.y < yBorder)
         {
             transform.position = new Vector2(transform.position.x, transform.position.y + ySpeed);
         }
-        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -yBorder)
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -yBorder)
         {
             transform.position = new Vector2(transform.position.x, transform.position.y - ySpeed);
         }
